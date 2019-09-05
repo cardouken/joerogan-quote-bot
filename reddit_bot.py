9b006b7a8b4c6a4deb39eeedfc6a2f341c032cb9
cardo <cardo@uustal.ee>
2019-09-05 13:57:24 +0300

    added PMing back with a quote

--- reddit_bot.py
+++ reddit_bot.py
@@ -20,24 +20,39 @@ def bot_login():
     return r
 
 
-if not os.path.isfile("/resources/posts_replied_to.txt"):
+if not os.path.isfile("resources/posts_replied_to.txt"):
     posts_replied_to = []
 
 else:
     # Read the file into a list and remove any empty values
-    with open("/resources/posts_replied_to.txt", "r") as f:
+    with open("resources/posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
 
 
 def run_bot(r):
+    with open("resources/list.txt") as file:
+        phrases = file.readlines()
+        random_phrase = random.choice(phrases)
+
+        for pm in r.inbox.unread():
+            frm = pm.author
+            sub = pm.subject
+
+            print(frm)
+            print(sub)
+
+            repsub = 'Re: ' + sub
+            msg = ">\"*" + random_phrase.strip() + "*\" \n\n ^Joe ^Rogan"
+
+            pm.author.message(repsub, msg)
+            pm.mark_read()
+
     for comment in r.subreddit('joerogan+picmipbotplayground').comments(limit=25):
         if comment.id not in posts_replied_to and "!joe" in comment.body.lower():
             print("String with \"!joe\" found in comment " + comment.id)
-            with open("/resources/list.txt") as file:
-                phrases = file.readlines()
-            random_phrase = random.choice(phrases)
+
             try:
                 comment.reply(">\"*" + random_phrase.strip() + "*\" \n\n ^Joe ^Rogan")
                 print("Replied to comment " + comment.id + " with " + "\"" + random_phrase.strip() + "\"")
@@ -49,7 +64,7 @@ def run_bot(r):
     print("Sleeping for 10 seconds")
     time.sleep(10)
 
-    with open("posts_replied_to.txt", "w") as f:
+    with open("resources/posts_replied_to.txt", "w") as f:
         for post_id in posts_replied_to:
             f.write(post_id + "\n")
 

809a2fde822e7f00e737d4d3e070e1e88c81be56
cardo <cardo@uustal.ee>
2019-09-05 11:19:18 +0300

    quick structure update

--- reddit_bot.py
+++ reddit_bot.py
@@ -21,13 +21,13 @@ def bot_login():
 
 
 # Have we run this code before? If not, create an empty list
-if not os.path.isfile("posts_replied_to.txt"):
+if not os.path.isfile("/resources/posts_replied_to.txt"):
     posts_replied_to = []
 
 # If we have run the code before, load the list of posts we have replied to
 else:
     # Read the file into a list and remove any empty values
-    with open("posts_replied_to.txt", "r") as f:
+    with open("/resources/posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
@@ -37,7 +37,7 @@ def run_bot(r):
     for comment in r.subreddit('joerogan+picmipbotplayground').comments(limit=25):
         if comment.id not in posts_replied_to and "dmt" in comment.body.lower():
             print("String with \"!joe\" found in comment " + comment.id)
-            with open("list.txt") as file:
+            with open("/resources/list.txt") as file:
                 phrases = file.readlines()
             random_phrase = random.choice(phrases)
             try:

d14d9e5ce5d5dfc80a5d93c8d9d725e7ddb6b753
Cardo Uustal <cardo@uustal.ee>
2020-02-07 06:47:58 +0200

    remove responding to private messages for the time being

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,14 +1,12 @@
 import csv
 import datetime
+import os
+import random
+import time
 import traceback
 
 import praw
 from praw.exceptions import APIException
-from praw.models import Message
-
-import time
-import random
-import os
 
 if os.environ.get('reddit_username') == 'ckypop':
     devprod = "(DEV MODE)"
@@ -44,23 +42,9 @@ def bot_login():
 
 
 def run_bot(r):
-    def check_pm():
-        for pm in r.inbox.unread():
-            if isinstance(pm, Message):
-                frm = pm.author
-                sub = pm.subject
-
-                print("PM from: ", frm)
-                print("Subject: ", sub)
-
-                repsub = 'Re: ' + sub
-                random_phrase_pm = random.choice(phrases)
-                msg = ">\"*" + random_phrase_pm.strip() + "*\" \n\n ^Joe ^Rogan"
-                pm.author.message(repsub, msg)
-                pm.mark_read()
-                print("Replied to PM with:", "\"", random_phrase_pm.strip(), "\"")
-
     for comment in r.subreddit(os.environ['active_subreddit']).stream.comments():
+        print("test")
+
         def save_posts():
             posts[comment.id] = time.time()
             wr = csv.writer(open("resources/posts.csv", "w"))
@@ -82,46 +66,37 @@ def run_bot(r):
                 secondselapsed = (last_replied_post_timestamp + cooldown) - time.time()
                 return secondselapsed > 0
 
-        def get_cooldown_time_remaining(author):
+        def remaining_cooldown(author):
             if author in authors:
                 timestamp = authors[author]
                 return int(cooldown - (time.time() - timestamp))
 
-        def should_reply():
-            return post_not_replied(posts, comment.id) \
-                   and "!joe" in comment.body.lower() \
-                   and comment_is_new()
+        if post_not_replied(posts, comment.id) and "!joe" in comment.body.lower():
+            if comment.created_utc > time.time() - 30:
+                if check_author_cooldown_status(comment.author):
+                    print(str(comment.author), "posted comment", comment.id, "but is in cooldown for another",
+                          remaining_cooldown(comment.author), "seconds, doing nothing")
+                else:
+                    print("Comment containing \"!joe\" posted by", str(comment.author), "to",
+                          "https://reddit.com" + comment.submission.permalink + comment.id)
+                    try:
+                        random_phrase = random.choice(phrases)
+                        comment.reply(">\"*" + random_phrase.strip() + "*\" \n\n ^Joe ^Rogan")
+                        print("Replied to comment ", comment.id, "with", "\"", random_phrase.strip(), "\"")
+                    except APIException as e:
+                        traceback.print_exc(e)
 
-        def comment_is_new():
-            return comment.created_utc > time.time() - 30
+                    save_cooldown()
+                    save_posts()
 
-        if should_reply():
-            if check_author_cooldown_status(comment.author):
-                print(str(comment.author), "posted comment", comment.id, "but is in cooldown for another",
-                      get_cooldown_time_remaining(comment.author), "seconds, doing nothing")
-                pass
-            else:
-                print("Comment containing \"!joe\" posted by", str(comment.author), "to",
-                      "https://reddit.com" + comment.submission.permalink + comment.id)
-                try:
-                    random_phrase = random.choice(phrases)
-                    comment.reply(">\"*" + random_phrase.strip() + "*\" \n\n ^Joe ^Rogan")
-                    print("Replied to comment ", comment.id, "with", "\"", random_phrase.strip(), "\"")
-                except APIException as e:
-                    traceback.print_exc(e)
-
-                save_cooldown()
+            elif comment.created_utc < time.time() - 30:
+                now = int(time.time())
+                then = int(comment.created_utc)
+                delta = now - then
+                print(comment.id, "posted",
+                      datetime.datetime.fromtimestamp(then).strftime('%H:%M:%S %d/%m/%Y') + ",",
+                      delta, "seconds or", round(delta / 60, 1), "minutes ago")
                 save_posts()
-
-        elif should_reply():
-            now = int(time.time())
-            then = int(comment.created_utc)
-            delta = now - then
-            print(comment.id, "posted", datetime.datetime.fromtimestamp(then).strftime('%H:%M:%S %d/%m/%Y') + ",",
-                  delta, "seconds or", round(delta / 60, 1), "minutes ago")
-            save_posts()
-
-        check_pm()
 
 
 reddit = bot_login()

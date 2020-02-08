ecae1e34774b3ab8d678c552c8931e82b8f664e8
Cardo Uustal <cardo@uustal.ee>
2020-02-08 04:19:13 +0200

    refactor a bit more, bugfixes

--- reddit_bot.py
+++ reddit_bot.py
@@ -8,57 +8,58 @@ import praw
 from praw.exceptions import APIException
 from praw.models import Message
 
-authors = {}
 blacklist = {}
-cooldown = 300
+posts = {}
+cooldowns = {}
+keywords = []
+phrases = []
+
+if os.environ.get('reddit_username') != 'jamiepullthatquote':
+    cooldown_time = int(os.environ['cooldown_dev'])
+    dev_env = "(DEVELOPMENT MODE)"
+else:
+    cooldown_time = int(os.environ['cooldown_time'])
+    dev_env = "(PRODUCTION MODE)"
 
 
 def main():
-    if os.environ.get('reddit_username') != 'jamiepullthatquote':
-        devprod = "(DEVELOPMENT MODE)"
-    else:
-        devprod = "(PRODUCTION MODE)"
-
-    get_posts()
-    get_phrases()
-    get_triggers()
-
     print("Currently listening in:", os.environ['active_subreddit'])
-    print("Logging in as", os.environ.get('reddit_username'), devprod)
+    print("Logging in as", os.environ.get('reddit_username'), dev_env)
     r = praw.Reddit(username=os.environ['reddit_username'],
                     password=os.environ['reddit_password'],
                     client_id=os.environ['client_id'],
                     client_secret=os.environ['client_secret'],
-                    user_agent="Joe Rogan quote responder:v1.0.1 (by /u/picmip)")
-    print("Logged in as", os.environ.get('reddit_username'), devprod)
-    run_bot(r)
+                    user_agent="Joe Rogan quote responder:v1.1")
+    print("Logged in as", os.environ.get('reddit_username'), dev_env)
+    load_resources()
+    check_comments(r)
 
 
-def run_bot(r):
+def check_comments(r):
     for comment in r.subreddit(os.environ['active_subreddit']).stream.comments():
         check_pm(r)
         user = comment.author
-        cbody = comment.body.lower()
         user_not_self = user != os.environ.get('reddit_username')
+        comment_body = comment.body.lower()
         comment_newer_than_30sec = comment.created_utc > time.time() - 30
         comment_url = "https://reddit.com" + comment.submission.permalink + comment.id
-        comment_has_keyword_without_reply = comment.id not in posts.keys() and keyword_found_in_comment(cbody)
+        comment_has_keyword_without_reply = comment.id not in posts.keys() and keyword_found_in_comment(comment_body)
 
         if comment_has_keyword_without_reply and comment_newer_than_30sec and user_not_self:
             if user_blacklisted(user):
                 print(user, "has asked to be blacklisted")
             elif user_in_cooldown(user):
-                print(user, "posted", comment.id, "but is in cooldown for", remaining_cooldown(user), "seconds")
+                print(user, "posted", comment_url, "but is in cooldown for", remaining_cooldown(user))
             else:
-                print("Keyword found posted by", user, "at", comment_url)
+                print("Keyword found, posted by", user, "at", comment_url)
                 try:
-                    if "!joe" in cbody:
+                    if "!joe" in comment_body:
                         phrase = random.choice(phrases)
                         comment_reply(comment, phrase.strip())
                         print("Replied to comment", comment.id, "with:", phrase.strip())
                     else:
                         phrases_arr = []
-                        find_keyword_in_comment(cbody, phrases_arr)
+                        find_keyword_in_comment(comment_body, phrases_arr)
                         phrase = random.choice(phrases_arr)
                         comment_reply(comment, phrase)
                         print("Replied to comment", comment.id, "with:", phrase)
@@ -73,78 +74,19 @@ def run_bot(r):
 def check_pm(r):
     for pm in r.inbox.unread():
         if isinstance(pm, Message):
-            frm = pm.author
-            sub = pm.subject
-            msg = pm.body
+            user = pm.author
+            subject = pm.subject
+            message = pm.body
 
-            print("PM from:", frm, "Subject:", sub, "Message:", msg)
-            repsub = 'Re: ' + sub
+            print("PM from:", user, "Subject:", subject, "Message:", message)
+            reply_subject = 'Re: ' + subject
 
-            if "fuck off" in msg or "fuck off" in sub:
-                blacklist_user(frm)
-                reply = "you have been unsubscribed from joe rogan facts and will not be replied to again"
-                frm.message(repsub, reply)
+            if "fuck off" in message or "fuck off" in subject:
+                save_blacklist(user)
+                reply_message = "you have been unsubscribed from joe rogan facts and will not be replied to again"
+                user.message(reply_subject, reply_message)
                 pm.mark_read()
-                print("Replied to PM with:", "\"", reply, "\"")
-
-
-def save_posts(comment):
-    posts[comment.id] = time.time()
-    wr = csv.writer(open("resources/posts.csv", "w"))
-    for k, v in posts.items():
-        wr.writerow([k, v])
-
-
-def save_cooldown(comment):
-    authors[comment.author] = time.time()
-    wr = csv.writer(open("resources/cooldowns.csv", "w"))
-    for k, v in authors.items():
-        wr.writerow([k, v])
-
-
-def clear_authors(comment):
-    authors.clear()
-    save_cooldown(comment)
-
-
-def clear_blacklist():
-    blacklist.clear()
-
-
-def user_in_cooldown(author):
-    if author in authors:
-        last_replied_post_timestamp = authors[author]
-        seconds_elapsed = (last_replied_post_timestamp + cooldown) - time.time()
-        return seconds_elapsed > 0
-
-
-def user_blacklisted(author):
-    return author in blacklist
-
-
-def remaining_cooldown(author):
-    if author in authors:
-        timestamp = authors[author]
-        return int(cooldown - (time.time() - timestamp))
-
-
-def blacklist_user(frm):
-    blacklist[frm] = time.time()
-    w = csv.writer(open("resources/blacklist.csv", "w"))
-    for k, v in blacklist.items():
-        w.writerow([k, v])
-
-
-def keyword_found_in_comment(cbody):
-    return any(word.lower() in cbody for word in triggers or "!joe" in cbody)
-
-
-def find_keyword_in_comment(cbody, phrases_arr):
-    for trigger in triggers:
-        if trigger.lower() in cbody:
-            for phrase in phrases:
-                if trigger.lower() in phrase.lower():
-                    phrases_arr.append(phrase)
+                print("Replied to PM with:", "\"", reply_message, "\"")
 
 
 def comment_reply(comment, phrase):
@@ -158,9 +100,81 @@ def comment_reply(comment, phrase):
             'reddit_username') + "&subject=fuck%20off&message=fuck%20off)")
 
 
-def get_posts():
-    global posts, line
-    posts = {}
+def keyword_found_in_comment(comment_body):
+    return any(word.lower() in comment_body for word in keywords or "!joe" in comment_body)
+
+
+def find_keyword_in_comment(comment_body, phrases_arr):
+    for word in keywords:
+        if word.lower() in comment_body:
+            for phrase in phrases:
+                if word.lower() in phrase.lower():
+                    phrases_arr.append(phrase)
+
+
+def save_posts(comment):
+    posts[comment.id] = time.time()
+    wr = csv.writer(open("resources/posts.csv", "w"))
+    for k, v in posts.items():
+        wr.writerow([k, v])
+
+
+def save_cooldown(comment):
+    cooldowns[comment.author] = time.time()
+    wr = csv.writer(open("resources/cooldowns.csv", "w"))
+    for k, v in cooldowns.items():
+        wr.writerow([k, v])
+
+
+def save_blacklist(user):
+    blacklist[user] = time.time()
+    w = csv.writer(open("resources/blacklist.csv", "w"))
+    for k, v in blacklist.items():
+        w.writerow([k, v])
+
+
+def user_in_cooldown(author):
+    if author in cooldowns:
+        last_replied_post_timestamp = cooldowns[author]
+        seconds_elapsed = (last_replied_post_timestamp + cooldown_time) - time.time()
+        return seconds_elapsed > 0
+
+
+def remaining_cooldown(author):
+    if author in cooldowns:
+        timestamp = cooldowns[author]
+        return str(int(cooldown_time - (time.time() - timestamp))) + " seconds"
+
+
+def user_blacklisted(author):
+    return author in blacklist
+
+
+def clear_cooldowns(comment):
+    cooldowns.clear()
+    save_cooldown(comment)
+
+
+def clear_blacklist():
+    blacklist.clear()
+
+
+def load_resources():
+    with open("resources/cooldowns.csv") as file:
+        for line in file:
+            if ',' not in line:
+                continue
+            (key, val) = line.strip().split(',')
+            cooldowns[key] = val
+
+    with open("resources/keywords.txt") as file:
+        for line in file:
+            keywords.append(line.strip())
+
+    with open("resources/list.txt") as file:
+        for line in file:
+            phrases.append(line.strip())
+
     with open("resources/posts.csv") as f:
         for line in f:
             if ',' not in line:
@@ -168,21 +182,13 @@ def get_posts():
             (key, val) = line.strip().split(',')
             posts[key] = val
 
-
-def get_phrases():
-    global phrases, file, line
-    phrases = []
-    with open("resources/list.txt") as file:
+    with open("resources/blacklist.csv") as file:
         for line in file:
-            phrases.append(line.strip())
-
-
-def get_triggers():
-    global triggers, file, line
-    triggers = []
-    with open("resources/triggers.txt") as file:
-        for line in file:
-            triggers.append(line.strip())
+            if ',' not in line:
+                continue
+            (key, val) = line.strip().split(',')
+            blacklist[key] = val
+            print("Blacklist:", blacklist)
 
 
 if __name__ == "__main__":
